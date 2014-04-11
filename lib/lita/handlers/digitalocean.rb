f02731af89ab0ae0d8f5b895498f41562b9638bb
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-04-11 00:55:44 -0700

    Add ssh keys add.

--- lib/lita/handlers/digitalocean.rb
+++ lib/lita/handlers/digitalocean.rb
@@ -24,6 +24,10 @@ module Lita
         t("help.ssh_keys.show_key") => t("help.ssh_keys.show_value"),
       }
 
+      do_route /^do\s+ssh\s+keys?\s+add\s+.+$/i, :ssh_keys_add, {
+        t("help.ssh_keys.add_key") => t("help.ssh_keys.add_value")
+      }
+
       def ssh_keys_list(response)
         do_response = do_call(response) do |client|
           client.ssh_keys.list
@@ -45,6 +49,23 @@ module Lita
 
         key = do_response.ssh_key
         response.reply("#{key.id} (#{key.name}): #{key.ssh_pub_key}")
+      end
+
+      def ssh_keys_add(response)
+        name, public_key = response.args[3..4]
+
+        unless name && public_key
+          return response.reply("#{t('format')}: #{t('help.ssh_keys.add_key')}")
+        end
+
+        do_response = do_call(response) do |client|
+          client.ssh_keys.add(name: name, ssh_pub_key: public_key)
+        end or return
+
+        key = do_response.ssh_key
+        response.reply(
+          t("ssh_keys.add.created", message: "#{key.id} (#{key.name}): #{key.ssh_pub_key}")
+        )
       end
 
       private
--- spec/lita/handlers/digitalocean_spec.rb
+++ spec/lita/handlers/digitalocean_spec.rb
@@ -4,6 +4,7 @@ describe Lita::Handlers::Digitalocean, lita_handler: true do
   it { routes_command("do ssh keys list").to(:ssh_keys_list) }
   it { routes_command("do ssh key list").to(:ssh_keys_list) }
   it { routes_command("do ssh keys show 123").to(:ssh_keys_show) }
+  it { routes_command("do ssh keys add 'foo bar' 'ssh-rsa abcdefg'").to(:ssh_keys_add) }
 
   let(:client) { instance_double("::DigitalOcean::API") }
 
@@ -72,6 +73,32 @@ describe Lita::Handlers::Digitalocean, lita_handler: true do
       allow(client).to receive_message_chain(:ssh_keys, :show).and_return(do_response)
       send_command("do ssh keys show 123")
       expect(replies.last).to eq("123 (My Key): ssh-rsa abcdefg")
+    end
+  end
+
+  describe "#ssh_keys_add" do
+    let(:do_response) do
+      instance_double(
+        "Hashie::Rash",
+        status: "OK",
+        ssh_key: instance_double(
+          "Hashie::Rash",
+          id: 123,
+          name: "My Key",
+          ssh_pub_key: "ssh-rsa abcdefg"
+        )
+      )
+    end
+
+    it "responds with the details of the new key" do
+      allow(client).to receive_message_chain(:ssh_keys, :add).and_return(do_response)
+      send_command("do ssh keys add 'My Key' 'ssh-rsa abcdefg'")
+      expect(replies.last).to eq("Created new SSH key: 123 (My Key): ssh-rsa abcdefg")
+    end
+
+    it "responds with an error message if name or public key is missing" do
+      send_command("do ssh keys add foo")
+      expect(replies.last).to eq("Format: do ssh keys add NAME PUBLIC_KEY")
     end
   end
 end
