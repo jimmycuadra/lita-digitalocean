47e7c1301f9a3967e8ebd5c7f16254f45ad23130
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-04-11 01:01:42 -0700

    Remove duplication.

--- spec/lita/handlers/digitalocean_spec.rb
+++ spec/lita/handlers/digitalocean_spec.rb
@@ -22,8 +22,8 @@ describe Lita::Handlers::Digitalocean, lita_handler: true do
     allow(::DigitalOcean::API).to receive(:new).and_return(client)
   end
 
-  describe "#ssh_keys_list" do
-    let(:do_response_success) do
+  describe "ssh key commands" do
+    let(:do_list) do
       instance_double(
         "Hashie::Rash",
         status: "OK",
@@ -34,7 +34,7 @@ describe Lita::Handlers::Digitalocean, lita_handler: true do
       )
     end
 
-    let(:do_response_empty) do
+    let(:do_list_empty) do
       instance_double(
         "Hashie::Rash",
         status: "OK",
@@ -42,21 +42,7 @@ describe Lita::Handlers::Digitalocean, lita_handler: true do
       )
     end
 
-    it "returns a list of key IDs and names" do
-      allow(client).to receive_message_chain(:ssh_keys, :list).and_return(do_response_success)
-      send_command("do ssh keys list")
-      expect(replies).to eq(["123 (My Key)", "456 (Your Key)"])
-    end
-
-    it "returns an empty state message if there are no SSH keys" do
-      allow(client).to receive_message_chain(:ssh_keys, :list).and_return(do_response_empty)
-      send_command("do ssh keys list")
-      expect(replies.last).to eq("No SSH keys have been added yet.")
-    end
-  end
-
-  describe "#ssh_keys_show" do
-    let(:do_response) do
+    let(:do_key) do
       instance_double(
         "Hashie::Rash",
         status: "OK",
@@ -69,36 +55,39 @@ describe Lita::Handlers::Digitalocean, lita_handler: true do
       )
     end
 
-    it "responds with the public key" do
-      allow(client).to receive_message_chain(:ssh_keys, :show).and_return(do_response)
-      send_command("do ssh keys show 123")
-      expect(replies.last).to eq("123 (My Key): ssh-rsa abcdefg")
-    end
-  end
+    describe "#ssh_keys_list" do
+      it "returns a list of key IDs and names" do
+        allow(client).to receive_message_chain(:ssh_keys, :list).and_return(do_list)
+        send_command("do ssh keys list")
+        expect(replies).to eq(["123 (My Key)", "456 (Your Key)"])
+      end
 
-  describe "#ssh_keys_add" do
-    let(:do_response) do
-      instance_double(
-        "Hashie::Rash",
-        status: "OK",
-        ssh_key: instance_double(
-          "Hashie::Rash",
-          id: 123,
-          name: "My Key",
-          ssh_pub_key: "ssh-rsa abcdefg"
-        )
-      )
+      it "returns an empty state message if there are no SSH keys" do
+        allow(client).to receive_message_chain(:ssh_keys, :list).and_return(do_list_empty)
+        send_command("do ssh keys list")
+        expect(replies.last).to eq("No SSH keys have been added yet.")
+      end
     end
 
-    it "responds with the details of the new key" do
-      allow(client).to receive_message_chain(:ssh_keys, :add).and_return(do_response)
-      send_command("do ssh keys add 'My Key' 'ssh-rsa abcdefg'")
-      expect(replies.last).to eq("Created new SSH key: 123 (My Key): ssh-rsa abcdefg")
+    describe "#ssh_keys_show" do
+      it "responds with the public key" do
+        allow(client).to receive_message_chain(:ssh_keys, :show).and_return(do_key)
+        send_command("do ssh keys show 123")
+        expect(replies.last).to eq("123 (My Key): ssh-rsa abcdefg")
+      end
     end
 
-    it "responds with an error message if name or public key is missing" do
-      send_command("do ssh keys add foo")
-      expect(replies.last).to eq("Format: do ssh keys add NAME PUBLIC_KEY")
+    describe "#ssh_keys_add" do
+      it "responds with the details of the new key" do
+        allow(client).to receive_message_chain(:ssh_keys, :add).and_return(do_key)
+        send_command("do ssh keys add 'My Key' 'ssh-rsa abcdefg'")
+        expect(replies.last).to eq("Created new SSH key: 123 (My Key): ssh-rsa abcdefg")
+      end
+
+      it "responds with an error message if name or public key is missing" do
+        send_command("do ssh keys add foo")
+        expect(replies.last).to eq("Format: do ssh keys add NAME PUBLIC_KEY")
+      end
     end
   end
 end
